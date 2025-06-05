706520123a30fa38e5c4821b7865f2ccbe3725d4
fedebacci <federicobacci1999@gmail.com>
2025-06-05 12:52:57 +0200

    setup variabili di sistema

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,7 +1,12 @@
 export default function Main () {
+
+    const appName = import.meta.env.VITE_APP_NAME;
+
     return (
         <div className="container">
-            <h1>APP</h1>
+            <h1>
+                {appName}
+            </h1>
         </div>
     );
 };
