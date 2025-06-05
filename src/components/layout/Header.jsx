163326706f943d7860bf31915438be1c5a8bd42b
fedebacci <federicobacci1999@gmail.com>
2025-06-05 14:12:48 +0200

    Milestone 1 - gestione base del form di ricerca con React

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,22 +1,54 @@
-export default function Header () {
-    return (
-    <header>
-        <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    Boolflix
-                </a>
+import { useState } from "react";
 
-                <form className="d-flex" role="search">
-                    <div className="input-group">
-                        <button className="btn btn-outline-success" type="submit">
-                            Cerca
-                        </button>
-                        <input className="form-control me-2" type="search" placeholder="Cerca" aria-label="Cerca"/>
-                    </div>
-                </form>
-            </div>
-        </nav>
-    </header>
+
+
+
+export default function Header () {
+
+    // todo: Trasformare in oggetto per ricerca testo E genere
+    const [searchData, setSearchData] = useState("");
+    // todo: Modificare per riconoscere quale prop è stata cambiata tramite attributo "name" (text o genre) per ricerca testo E genere
+    const handleInputChange = (e) => {
+        setSearchData(e.target.value);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.info(searchData);
+    };
+
+
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        Boolflix
+                    </a>
+
+                    <form className="d-flex" role="search">
+                        <div className="input-group">
+                            <button 
+                                onClick={handleFormSubmit}
+
+                                className="btn btn-outline-success" 
+                                type="submit"
+                            >
+                                Cerca
+                            </button>
+                            <input 
+                                value={searchData}
+                                onChange={handleInputChange}
+
+                                className="form-control me-2" 
+                                type="search" 
+                                placeholder="Cerca" 
+                                aria-label="Cerca"
+                            />
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </header>
     );
 };
