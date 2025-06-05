16a4b695f2ed233b5de5663c9af3915b83743685
fedebacci <federicobacci1999@gmail.com>
2025-06-05 14:03:44 +0200

    setup della struttura di base necessaria per poter iniziare con il Milestone 1 dell'esercizio

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,22 @@
 export default function Header () {
     return (
-        <div className="container">
-            <h1>
-                Header
-            </h1>
-        </div>
+    <header>
+        <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    Boolflix
+                </a>
+
+                <form className="d-flex" role="search">
+                    <div className="input-group">
+                        <button className="btn btn-outline-success" type="submit">
+                            Cerca
+                        </button>
+                        <input className="form-control me-2" type="search" placeholder="Cerca" aria-label="Cerca"/>
+                    </div>
+                </form>
+            </div>
+        </nav>
+    </header>
     );
 };
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -3,10 +3,12 @@ export default function Main () {
     const appName = import.meta.env.VITE_APP_NAME;
 
     return (
-        <div className="container">
-            <h1>
-                {appName}
-            </h1>
-        </div>
+        <section className="py-5">
+            <div className="container">
+                <h1>
+                    {appName}
+                </h1>
+            </div>
+        </section>
     );
 };
